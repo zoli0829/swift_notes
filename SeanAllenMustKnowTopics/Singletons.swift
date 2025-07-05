b0f0d3f83d637a748f69877696174d28c3351b07
zoli0829 <zoli0829@gmail.com>
2025-07-05 15:32:56 +0100

    Singletons

--- SeanAllenMustKnowTopics/Singletons.swift
+++ SeanAllenMustKnowTopics/Singletons.swift
@@ -1,1 +1,13 @@
 // Singletons
+// An instance of a class that can only be created once and is globally accessible throughout the app.
+// like UserDefaults.standard in iOS
+
+final class MySingleton {
+
+  static let shared = MySingleton()
+  private init() {}
+
+  func doSomething() {
+    print("Doing something in MySingleton")
+  }
+}
